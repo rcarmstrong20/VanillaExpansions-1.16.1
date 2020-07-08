e51f8351b2ea2873a75e51f64f79b1f5de4fe039
rcarmstrong20 <rcarmstrong20@gmail.com>
2020-07-08 14:30:21 -0600

    Fixed a bug where by holding down right-click you could get mass amounts of blood

--- src/main/java/rcarmstrong20/vanilla_expansions/item/VeGlassVialItem.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/item/VeGlassVialItem.java
@@ -1,10 +1,14 @@
 package rcarmstrong20.vanilla_expansions.item;
 
+import java.util.Arrays;
+import java.util.List;
+
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.GlassBottleItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.DamageSource;
@@ -12,8 +16,15 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.SoundEvents;
 import rcarmstrong20.vanilla_expansions.core.VeItems;
 
-public class VeGlassVialItem extends GlassBottleItem
+public class VeGlassVialItem extends Item
 {
+	private static final List<EntityType<?>> ANIMALS_THAT_GIVE_BLOOD = Arrays.asList(EntityType.SHEEP,
+																					 EntityType.CHICKEN,
+																					 EntityType.COW,
+																					 EntityType.RABBIT,
+																					 EntityType.PIG,
+																					 EntityType.MOOSHROOM);
+	
 	public VeGlassVialItem(Properties properties)
 	{
 		super(properties);
@@ -24,16 +35,18 @@ public class VeGlassVialItem extends GlassBottleItem
 	{
 		if(stack.getItem() == VeItems.glass_vial)
 		{
-			if(target instanceof MobEntity && target.getType() != EntityType.ENDER_DRAGON && target.getType() != EntityType.WITHER)
+			if(target instanceof AnimalEntity && target.getHealth() > 0.0 && ANIMALS_THAT_GIVE_BLOOD.contains(target.getType()))
 			{
-				MobEntity mobTarget = (MobEntity) target;
+				MobEntity animalTarget = (MobEntity) target;
 				
-				mobTarget.playSound(SoundEvents.ITEM_BOTTLE_FILL, 1.0F, 1.0F);
-				mobTarget.attackEntityFrom(DamageSource.GENERIC, 1);
-				
-				playerIn.addItemStackToInventory(new ItemStack(VeItems.blood_vial));
+				if(animalTarget.attackEntityFrom(DamageSource.GENERIC, 1))
+				{
+					playerIn.addItemStackToInventory(new ItemStack(VeItems.blood_vial));
+					animalTarget.playSound(SoundEvents.ITEM_BOTTLE_FILL, 1.0F, 1.0F);
+					stack.shrink(1);
+					return ActionResultType.CONSUME;
+				}
 			}
-			return ActionResultType.CONSUME;
 		}
 		return ActionResultType.PASS;
 	}
