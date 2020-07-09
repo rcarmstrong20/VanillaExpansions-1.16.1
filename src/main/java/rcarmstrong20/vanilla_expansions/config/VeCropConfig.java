6b284bcf3927448015cb15b6739f08583f6e6b2b
rcarmstrong20 <rcarmstrong20@gmail.com>
2020-07-09 15:23:51 -0600

    Removed the black list and some unused imports

--- src/main/java/rcarmstrong20/vanilla_expansions/config/VeCropConfig.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/config/VeCropConfig.java
@@ -1,15 +1,11 @@
 package rcarmstrong20.vanilla_expansions.config;
 
-import java.util.List;
-
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
-import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 
 public class VeCropConfig
 {
 	public static BooleanValue enableRightClickHarvesting;
-	public static ConfigValue<List<String>> rightClickHarvestBlackList;
 	
 	public static void init(ForgeConfigSpec.Builder server, ForgeConfigSpec.Builder client)
 	{
