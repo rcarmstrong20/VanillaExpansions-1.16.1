80a4af8471c9e3ab1991dd9b5ffc573589f7b4fc
rcarmstrong20 <rcarmstrong20@gmail.com>
2020-07-11 22:39:45 -0600

    Cleaned up the format for the feature config

--- src/main/java/rcarmstrong20/vanilla_expansions/config/VeFeatureGenConfig.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/config/VeFeatureGenConfig.java
@@ -8,80 +8,75 @@ public class VeFeatureGenConfig
 	public static BooleanValue enableBlueberryBushSpawns;
 	public static BooleanValue enableCranberryBushSpawns;
 	public static BooleanValue enableWitchsCradleSpawns;
-	
 	public static BooleanValue enableBigPurpleMushroomSpawns;
+	public static BooleanValue enableCabinSpawns;
 	
 	public static BooleanValue enableVoidLakeSpawns;
-	
-	public static BooleanValue enableCabinSpawns;
+	public static BooleanValue enableSnapdragonSpawns;
 	
 	public static void init(ForgeConfigSpec.Builder server, ForgeConfigSpec.Builder client)
 	{
-		server.comment("Vanilla Expansions Feature Configuration");
+		server.comment("Vanilla Expansions Feature Configuration")
+			  .push("ve_feature_config");
 		
-		server.comment("Bush Configuration")
-			  .push("ve_bush_config");
+		server.comment("Overworld Configuration")
+			  .push("ve_overworld_config");
 		
-		server.comment("Bush Spawn Configuration")
-			  .push("bush_spawn");
-		
-		enableBlueberryBushSpawns = server.comment(compileComment("blueberry bushes"))
+		enableBlueberryBushSpawns = server.comment(compileOverworldComment("blueberry bushes"))
 										  .translation("ve.configBushes.enableBlueberryBushSpawns")
 										  .worldRestart()
 										  .define("enable_blueberry_bush_spawns", true);
 		
-		enableCranberryBushSpawns = server.comment(compileComment("cranberry bushes"))
+		enableCranberryBushSpawns = server.comment(compileOverworldComment("cranberry bushes"))
 										 .translation("ve.configBushes.enableCranberryBushSpawns")
 										 .worldRestart()
 										 .define("enable_cranberry_bush_spawns", true);
 		
-		enableWitchsCradleSpawns = server.comment(compileComment("witch's cradles"))
+		enableWitchsCradleSpawns = server.comment(compileOverworldComment("witch's cradles"))
 										 .translation("ve.configBushes.enableWitchsCradleSpawns")
 										 .worldRestart()
 										 .define("enable_witchs_cradle_spawns", true);
 		
-		server.pop(2);
+		enableBigPurpleMushroomSpawns = server.comment(compileOverworldComment("big purple mushrooms"))
+				 							  .translation("ve.configMushroom.enableBigPurpleMushroomSpawns")
+				 							  .worldRestart()
+				 							  .define("enable_big_purple_mushroom_spawns", true);
 		
-		server.comment("Vanilla Expansions Mushroom Configuration")
-			   .push("ve_mushroom_config");
+		enableCabinSpawns = server.comment(compileOverworldComment("cabins"))
+				 				  .translation("ve.configStructure.enableCabinSpawns")
+				 				  .worldRestart()
+				 				  .define("enable_cabin_spawns", true);
 		
-		server.comment("Mushroom Spawn Configuration")
-			  .push("mushroom_spawn");
+		server.pop();
 		
-		enableBigPurpleMushroomSpawns = server.comment(compileComment("big purple mushrooms"))
-											  .translation("ve.configMushroom.enableBigPurpleMushroomSpawns")
-											  .worldRestart()
-											  .define("enable_big_purple_mushroom_spawns", true);
-		server.pop(2);
+		server.comment("End Configuration")
+			  .push("ve_end_config");
 		
-		server.comment("Vanilla Expansions Lake Configuration")
-			  .push("ve_lake_config");
-		
-		server.comment("Lake Spawn Configuration")
-			  .push("lake_spawn");
-		
-		enableVoidLakeSpawns = server.comment(compileComment("void lakes"))
+		enableVoidLakeSpawns = server.comment(compileEndComment("void lakes"))
 									 .translation("ve.configLake.enableVoidLakeSpawns")
 									 .worldRestart()
 									 .define("enable_void_lake_spawns", true);
-		server.pop(2);
 		
-		server.comment("Vanilla Expansions Structure Configuration")
-			  .push("ve_structure_config");
-		
-		server.comment("Structure Spawn Configuration")
-			  .push("structure_spawn");
-		
-		enableCabinSpawns = server.comment(compileComment("cabins"))
-								  .translation("ve.configStructure.enableCabinSpawns")
-								  .worldRestart()
-								  .define("enable_cabin_spawns", true);
+		enableSnapdragonSpawns = server.comment(compileEndComment("snapdragons"))
+									   .translation("ve.configLake.enableSnapdragonSpawns")
+									   .worldRestart()
+									   .define("enable_snapdragon_spawns", true);
 		
 		server.pop(2);
 	}
 	
-	private static String compileComment(String name)
+	private static String compileOverworldComment(String name)
 	{
-		return "If true " + name + " will spawn in the world.";
+		return compileComment(name, "overworld");
+	}
+	
+	private static String compileEndComment(String name)
+	{
+		return compileComment(name, "end");
+	}
+	
+	private static String compileComment(String name, String worldName)
+	{
+		return "If true " + name + " will spawn in the " + worldName + ".";
 	}
 }
