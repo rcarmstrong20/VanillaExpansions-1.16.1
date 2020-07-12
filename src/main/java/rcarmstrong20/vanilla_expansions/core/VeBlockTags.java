f00a4c8965bb72232ae5d33a006a4129ed07cfa3
rcarmstrong20 <rcarmstrong20@gmail.com>
2020-07-11 22:38:30 -0600

    Renamed the tags

--- src/main/java/rcarmstrong20/vanilla_expansions/core/VeBlockTags.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/core/VeBlockTags.java
@@ -7,10 +7,10 @@ import rcarmstrong20.vanilla_expansions.VanillaExpansions;
 
 public class VeBlockTags
 {
-	public static final ITag.INamedTag<Block> OVERWORLD_PLANTING_POTS = BlockTags.makeWrapperTag(VanillaExpansions.compileName(VanillaExpansions.MOD_ID, "overworld_planting_pots"));
-	public static final ITag.INamedTag<Block> NETHER_PLANTING_POTS = BlockTags.makeWrapperTag(VanillaExpansions.compileName(VanillaExpansions.MOD_ID, "nether_planting_pots"));
-	public static final ITag.INamedTag<Block> END_PLANTING_POTS = BlockTags.makeWrapperTag(VanillaExpansions.compileName(VanillaExpansions.MOD_ID, "end_planting_pots"));
-	public static final ITag.INamedTag<Block> OVERWORLD_PLANTS = BlockTags.makeWrapperTag(VanillaExpansions.compileName(VanillaExpansions.MOD_ID, "overworld_plants"));
-	public static final ITag.INamedTag<Block> NETHER_PLANTS = BlockTags.makeWrapperTag(VanillaExpansions.compileName(VanillaExpansions.MOD_ID, "nether_plants"));
-	public static final ITag.INamedTag<Block> END_PLANTS = BlockTags.makeWrapperTag(VanillaExpansions.compileName(VanillaExpansions.MOD_ID, "end_plants"));
+	public static final ITag.INamedTag<Block> OVERWORLD_PLANTABLE = BlockTags.makeWrapperTag(VanillaExpansions.compileName(VanillaExpansions.MOD_ID, "overworld_plantable"));
+	public static final ITag.INamedTag<Block> NETHER_PLANTABLE = BlockTags.makeWrapperTag(VanillaExpansions.compileName(VanillaExpansions.MOD_ID, "nether_plantable"));
+	public static final ITag.INamedTag<Block> END_PLANTABLE = BlockTags.makeWrapperTag(VanillaExpansions.compileName(VanillaExpansions.MOD_ID, "end_plantable"));
+	public static final ITag.INamedTag<Block> OVERWORLD_POTTABLE = BlockTags.makeWrapperTag(VanillaExpansions.compileName(VanillaExpansions.MOD_ID, "overworld_pottable"));
+	public static final ITag.INamedTag<Block> NETHER_POTTABLE = BlockTags.makeWrapperTag(VanillaExpansions.compileName(VanillaExpansions.MOD_ID, "nether_pottable"));
+	public static final ITag.INamedTag<Block> END_POTTABLE = BlockTags.makeWrapperTag(VanillaExpansions.compileName(VanillaExpansions.MOD_ID, "end_pottable"));
 }
