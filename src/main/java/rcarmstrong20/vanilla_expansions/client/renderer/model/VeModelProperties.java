6475a754090a8b216c721beac308d9b6132acc05
rcarmstrong20 <rcarmstrong20@gmail.com>
2020-07-13 16:19:51 -0600

    Removed unused files and removed the sleeping bag and double slab code.

--- src/main/java/rcarmstrong20/vanilla_expansions/client/renderer/model/VeModelProperties.java
+++
@@ -1,9 +0,0 @@
-package rcarmstrong20.vanilla_expansions.client.renderer.model;
-
-import net.minecraftforge.client.model.data.ModelProperty;
-import rcarmstrong20.vanilla_expansions.tile_entity.VeDoubleSlabTileEntity;
-
-public class VeModelProperties
-{
-	public static final ModelProperty<VeDoubleSlabTileEntity> TILE_ENTITY_DOUBLE_SLAB = new ModelProperty<VeDoubleSlabTileEntity>();
-}
