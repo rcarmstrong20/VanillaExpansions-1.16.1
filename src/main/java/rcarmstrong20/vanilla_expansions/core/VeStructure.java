c4ef192c8bf5a04acee0b4e09b5e85c82b7f6c6b
rcarmstrong20 <rcarmstrong20@gmail.com>
2020-12-01 21:08:14 -0700

    Continued trying to get the cabins to spawn.

--- src/main/java/rcarmstrong20/vanilla_expansions/core/VeStructure.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/core/VeStructure.java
@@ -1,51 +1,112 @@
 package rcarmstrong20.vanilla_expansions.core;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.structure.Structure;
-import net.minecraft.world.gen.feature.structure.VillageConfig;
+import net.minecraft.world.gen.settings.StructureSeparationSettings;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import rcarmstrong20.vanilla_expansions.VanillaExpansions;
 import rcarmstrong20.vanilla_expansions.gen.feature.structure.VeCabinStructure;
 
-// @Mod.EventBusSubscriber(modid = VanillaExpansions.MOD_ID, bus =
-// Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(modid = VanillaExpansions.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class VeStructure
 {
-    private static final List<Structure<?>> STRUCTURES = new ArrayList<>();
+    public static final List<Structure<?>> STRUCTURES = new ArrayList<>();
 
-    public static Structure<VillageConfig> cabin = register("cabin",
-            new VeCabinStructure(VillageConfig.field_236533_a_), GenerationStage.Decoration.SURFACE_STRUCTURES);
+    public static Structure<NoFeatureConfig> cabin = registerVillage("cabin",
+            new VeCabinStructure(NoFeatureConfig.field_236558_a_));
 
     /**
-     * @param <F>
-     * @param name       The name for the structure.
-     * @param structure  A new instance of the Structure<config> class.
-     * @param decoration The decoration category for this structure.
-     * @return A new structure.
+     * Helper method for registering all structures
      */
-    private static <F extends Structure<?>> F register(String name, F structure, GenerationStage.Decoration decoration)
+    private static Structure<NoFeatureConfig> registerVillage(String name, Structure<NoFeatureConfig> structure)
     {
-        Structure.NAME_STRUCTURE_BIMAP.put((VanillaExpansions.MOD_ID + ":" + name), structure);
+        VanillaExpansions.LOGGER.info("Structures registered.");
+
         structure.setRegistryName(VanillaExpansions.MOD_ID, name);
         STRUCTURES.add(structure);
         return structure;
     }
 
-    /**
-     * Register the Structures to the game.
-     *
-     * @param event An instance of the forge structure registry event.
-     */
     @SubscribeEvent
-    public static void registerStructures(final RegistryEvent.Register<Structure<?>> event)
+    public static void registerBlocks(final RegistryEvent.Register<Structure<?>> event)
     {
         STRUCTURES.forEach(structure -> event.getRegistry().register(structure));
+        STRUCTURES.forEach(
+                structure -> Structure.NAME_STRUCTURE_BIMAP.put(structure.getRegistryName().toString(), structure));
         STRUCTURES.clear();
+        // register();
 
-        VanillaExpansions.LOGGER.info("Structures registered.");
+        VanillaExpansions.LOGGER.info("Blocks registered.");
+    }
+
+    public static void register()
+    {
+        // StructureSeperationSettings(max spawn attempts (chunks), min spawn attempts
+        // (chunks), structure seed);
+
+        register(cabin, new StructureSeparationSettings(10, 5, 724628428), true);
+    }
+
+    /**
+     * Adds the provided structure to the registry, and adds the separation
+     * settings. The rarity of the structure is determined based on the values
+     * passed into this method in the structureSeparationSettings argument. Called
+     * by registerFeatures.
+     */
+    private static <F extends Structure<?>> void register(F structure, StructureSeparationSettings separationSettings,
+            boolean transformSurroundingLand)
+    {
+        /**
+         * Used in the locate command and will cause errors if our structures are not
+         * added.
+         */
+        Structure.NAME_STRUCTURE_BIMAP.put(structure.getRegistryName().toString(), structure);
+
+        /*
+         * Will add land at the base of the structure like it does for Villages and
+         * Outposts. Doesn't work well on structure that have pieces stacked vertically
+         * or change in heights.
+         */
+        if (transformSurroundingLand)
+        {
+            // Structure.field_236384_t_.add(structure);
+        }
+
+        try
+        {/*
+          * setFinalStatic(ObfuscationReflectionHelper.findField(
+          * DimensionStructuresSettings.class, "field_236191_b_"), false);
+          */
+            /*
+             * DimensionStructuresSettings.field_236191_b_ = ImmutableMap .<Structure<?>,
+             * StructureSeparationSettings>builder()
+             * .putAll(DimensionStructuresSettings.field_236191_b_).put(structure,
+             * separationSettings).build();
+             */
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void setFinalStatic(Field field, Object newValue) throws Exception
+    {
+        field.setAccessible(true);
+
+        // remove final modifier from field
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+        field.set(null, newValue);
     }
 }
--- src/main/java/rcarmstrong20/vanilla_expansions/gen/feature/structure/VeCabinPools.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/gen/feature/structure/VeCabinPools.java
@@ -7,13 +7,17 @@ import net.minecraft.world.gen.feature.jigsaw.JigsawPattern;
 import net.minecraft.world.gen.feature.jigsaw.JigsawPatternRegistry;
 import rcarmstrong20.vanilla_expansions.VanillaExpansions;
 
-public class CabinPools
+public class VeCabinPools
 {
-    public static final JigsawPattern taiga_cabin_pattern = JigsawPatternRegistry.func_244094_a(new JigsawPattern(
-            new ResourceLocation(VanillaExpansions.MOD_ID, "cabins/overworld/taiga_cabin"),
+    public static JigsawPattern taigaCabinPattern = JigsawPatternRegistry.func_244094_a(new JigsawPattern(
+            new ResourceLocation(VanillaExpansions.MOD_ID, "cabin/taiga_cabin_start_pool"),
             new ResourceLocation("empty"), ImmutableList.of(), JigsawPattern.PlacementBehaviour.TERRAIN_MATCHING));
 
-    public static final JigsawPattern forest_cabin_pattern = JigsawPatternRegistry.func_244094_a(new JigsawPattern(
+    public static JigsawPattern forestCabinPattern = JigsawPatternRegistry.func_244094_a(new JigsawPattern(
             new ResourceLocation(VanillaExpansions.MOD_ID, "cabins/overworld/forest_cabin"),
+            new ResourceLocation("empty"), ImmutableList.of(), JigsawPattern.PlacementBehaviour.TERRAIN_MATCHING));
+
+    public static JigsawPattern crimsonCabinPattern = JigsawPatternRegistry.func_244094_a(new JigsawPattern(
+            new ResourceLocation(VanillaExpansions.MOD_ID, "cabins/nether/crimson_cabin"),
             new ResourceLocation("empty"), ImmutableList.of(), JigsawPattern.PlacementBehaviour.TERRAIN_MATCHING));
 }
