06714fff3e9be48eb7a925d85af1db3af844f780
rcarmstrong20 <rcarmstrong20@gmail.com>
2020-12-17 14:31:37 -0700

    Optimized registry.

--- src/main/java/rcarmstrong20/vanilla_expansions/core/VeVillagerProfessions.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/core/VeVillagerProfessions.java
@@ -1,7 +1,5 @@
 package rcarmstrong20.vanilla_expansions.core;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,16 +11,11 @@ import net.minecraft.village.PointOfInterestType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import rcarmstrong20.vanilla_expansions.VanillaExpansions;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class VeVillagerProfessions
 {
-    private static final Constructor<VillagerProfession> VILLAGER_POFESSION_CONSTRUCTOR = ObfuscationReflectionHelper
-            .findConstructor(VillagerProfession.class, String.class, PointOfInterestType.class, ImmutableSet.class,
-                    ImmutableSet.class, SoundEvent.class);
-
     private static final List<VillagerProfession> VILLAGER_PROFESSIONS = new ArrayList<>();
 
     public static VillagerProfession lumberjack = register("lumberjack", VePointOfInterestTypes.lumberjack,
@@ -35,29 +28,20 @@ public class VeVillagerProfessions
      *                        workstation.
      * @return The new profession.
      */
-    private static VillagerProfession register(String name, PointOfInterestType pointOfInterest, SoundEvent sound)
+    private static VillagerProfession register(String name, PointOfInterestType poiType, SoundEvent sound)
     {
-        VillagerProfession profession = null;
-
-        try
-        {
-            profession = VILLAGER_POFESSION_CONSTRUCTOR.newInstance(VanillaExpansions.MOD_ID + ":" + name,
-                    pointOfInterest, ImmutableSet.of(), ImmutableSet.of(), sound);
-        }
-        catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
-        {
-            e.printStackTrace();
-        }
-
+        VillagerProfession profession = new VillagerProfession(VanillaExpansions.MOD_ID + ":" + name, poiType,
+                ImmutableSet.of(), ImmutableSet.of(), sound);
         profession.setRegistryName(VanillaExpansions.MOD_ID, name);
         VILLAGER_PROFESSIONS.add(profession);
+
         return profession;
     }
 
     @SubscribeEvent
     public static void registerVillagerProffesions(final RegistryEvent.Register<VillagerProfession> event)
     {
-        VILLAGER_PROFESSIONS.forEach(villager_profession -> event.getRegistry().register(villager_profession));
+        VILLAGER_PROFESSIONS.forEach(villagerProfession -> event.getRegistry().register(villagerProfession));
         VILLAGER_PROFESSIONS.clear();
         VanillaExpansions.LOGGER.info("Villager professions registered.");
     }
