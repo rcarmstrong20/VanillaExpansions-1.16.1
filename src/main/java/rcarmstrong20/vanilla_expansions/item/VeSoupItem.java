122ba6b2d45d607de40810ae5201cc0da723c742
rcarmstrong20 <rcarmstrong20@gmail.com>
2021-01-26 21:01:36 -0700

    Moved the return item to the constructor.

--- src/main/java/rcarmstrong20/vanilla_expansions/item/VeSoupItem.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/item/VeSoupItem.java
@@ -1,37 +1,34 @@
 package rcarmstrong20.vanilla_expansions.item;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.world.World;
-import rcarmstrong20.vanilla_expansions.core.VeItems;
 
 public class VeSoupItem extends Item
 {
+    private Item returnItem;
+
     public VeSoupItem(Properties properties)
     {
         super(properties);
+        this.returnItem = Items.BOWL;
+    }
+
+    public VeSoupItem(Properties properties, Item returnItem)
+    {
+        super(properties);
+        this.returnItem = returnItem;
     }
 
     @Override
-    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity livingEntityIn)
+    public ItemStack onItemUseFinish(ItemStack stackIn, World worldIn, LivingEntity livingEntityIn)
     {
-        super.onItemUseFinish(stack, worldIn, livingEntityIn);
-        Item item = this.getItem();
+        super.onItemUseFinish(stackIn, worldIn, livingEntityIn);
+        boolean flag = livingEntityIn instanceof PlayerEntity && ((PlayerEntity) livingEntityIn).isCreative();
 
-        if (item == VeItems.noodleSoup)
-        {
-            return new ItemStack(VeItems.noodleBowl);
-        }
-        else if (item == VeItems.darkMatterBucket)
-        {
-            return new ItemStack(Items.BUCKET);
-        }
-        else if (item == VeItems.caramelApple)
-        {
-            return new ItemStack(Items.STICK);
-        }
-        return new ItemStack(Items.BOWL);
+        return flag ? stackIn : new ItemStack(this.returnItem);
     }
 }
