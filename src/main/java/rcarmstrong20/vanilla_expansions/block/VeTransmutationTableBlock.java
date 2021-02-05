869b0f26699a8d56db4e3025150ba07d65a19e7a
rcarmstrong20 <rcarmstrong20@gmail.com>
2021-02-04 18:34:17 -0700

    Added the transmutation table.

---
+++ src/main/java/rcarmstrong20/vanilla_expansions/block/VeTransmutationTableBlock.java
@@ -0,0 +1,11 @@
+package rcarmstrong20.vanilla_expansions.block;
+
+import net.minecraft.block.Block;
+
+public class VeTransmutationTableBlock extends Block
+{
+    public VeTransmutationTableBlock(Properties properties)
+    {
+        super(properties);
+    }
+}
