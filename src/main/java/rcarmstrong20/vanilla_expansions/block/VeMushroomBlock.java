01b8fa0f6c9a51e3818e4e08f3d8b51c5afbf803
rcarmstrong20 <rcarmstrong20@gmail.com>
2021-02-22 18:16:08 -0700

    Added small to big purple mushroom functionality.

--- src/main/java/rcarmstrong20/vanilla_expansions/block/VeMushroomBlock.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/block/VeMushroomBlock.java
@@ -1,6 +1,15 @@
 package rcarmstrong20.vanilla_expansions.block;
 
+import java.util.Random;
+
+import net.minecraft.block.BlockState;
 import net.minecraft.block.MushroomBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Features;
+import net.minecraft.world.server.ServerWorld;
+import rcarmstrong20.vanilla_expansions.core.VeBlocks;
+import rcarmstrong20.vanilla_expansions.core.VeConfiguredFeatures;
 
 public class VeMushroomBlock extends MushroomBlock
 {
@@ -9,21 +18,25 @@ public class VeMushroomBlock extends MushroomBlock
         super(properties);
     }
 
-    /**
-     * Lets the small purple mushroom grow big when using bone meal.
-     */
-    /*
-     * @Override public boolean func_226940_a_(ServerWorld serverWorld, BlockPos
-     * blockPos, BlockState blockState, Random random) {
-     * serverWorld.removeBlock(blockPos, false);
-     * ConfiguredFeature<BigMushroomFeatureConfig, ?> configuredfeature; if (this ==
-     * VeBlocks.purple_mushroom) { configuredfeature =
-     * Feature.HUGE_RED_MUSHROOM.withConfiguration(VeBiomes.
-     * BIG_PURPLE_MUSHROOM_CONFIG); } else { return
-     * super.func_226940_a_(serverWorld, blockPos, blockState, random); } if
-     * (configuredfeature.func_242765_a(serverWorld,
-     * serverWorld.getChunkProvider().getChunkGenerator(), random, blockPos)) {
-     * return true; } return super.func_226940_a_(serverWorld, blockPos, blockState,
-     * random); }
-     */
+    @Override
+    public boolean grow(ServerWorld world, BlockPos pos, BlockState state, Random rand)
+    {
+        world.removeBlock(pos, false);
+        ConfiguredFeature<?, ?> hugeMushroom = Features.HUGE_RED_MUSHROOM;
+
+        if (this == VeBlocks.purpleMushroom)
+        {
+            hugeMushroom = VeConfiguredFeatures.HUGE_PURPLE_MUSHROOM;
+        }
+
+        if (hugeMushroom.generate(world, world.getChunkProvider().getChunkGenerator(), rand, pos))
+        {
+            return true;
+        }
+        else
+        {
+            world.setBlockState(pos, state, 3);
+            return false;
+        }
+    }
 }
