b580a4412fb4b55b5aa04d52ac7be29c0f52364a
rcarmstrong20 <rcarmstrong20@gmail.com>
2021-03-07 12:13:04 -0700

    improved comments and renamed a few methods.

--- src/main/java/rcarmstrong20/vanilla_expansions/core/VeStructure.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/core/VeStructure.java
@@ -8,6 +8,7 @@ import net.minecraft.world.gen.feature.structure.Structure;
 import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistry;
 import rcarmstrong20.vanilla_expansions.VanillaExpansions;
 import rcarmstrong20.vanilla_expansions.gen.feature.structure.VeCabinConfig;
 import rcarmstrong20.vanilla_expansions.gen.feature.structure.VeNetherCabinStructure;
@@ -23,20 +24,20 @@ public class VeStructure
 {
     public static final List<Structure<?>> STRUCTURES = new ArrayList<>();
 
-    public static Structure<VeCabinConfig> overworldCabin = registerStructure("overworld_cabin",
+    public static Structure<VeCabinConfig> overworldCabin = register("overworld_cabin",
             new VeOverworldCabinStructure(VeCabinConfig.cabinConfig));
-    public static Structure<VeCabinConfig> netherCabin = registerStructure("nether_cabin",
+    public static Structure<VeCabinConfig> netherCabin = register("nether_cabin",
             new VeNetherCabinStructure(VeCabinConfig.cabinConfig));
 
     /**
-     * Add and set the registry name for the new structure.
+     * Add this structure to the registry list and set its registry name.
      *
      * @param <C>       The feature config for the structure.
      * @param name      The name of the structure.
-     * @param structure An instance of a new structure.
+     * @param structure An instance for this new structure.
      * @return The new Structure.
      */
-    private static <C extends IFeatureConfig> Structure<C> registerStructure(String name, Structure<C> structure)
+    private static <C extends IFeatureConfig> Structure<C> register(String name, Structure<C> structure)
     {
         structure.setRegistryName(VanillaExpansions.MOD_ID, name);
         STRUCTURES.add(structure);
@@ -46,21 +47,22 @@ public class VeStructure
     @SubscribeEvent
     public static void registerStructures(final Register<Structure<?>> event)
     {
-        register(event, STRUCTURES);
+        register(event.getRegistry(), STRUCTURES);
     }
 
     /**
-     * A helper method that registers and adds them to the name structure bimap
-     * which is used in the locate command. Structures need a special registry.
+     * A helper method that registers and adds structures to the name structure
+     * bimap which is used in the locate command. For this reason the built-in forge
+     * registry for structures does not work.
      *
-     * @param event         The registry structure event to use.
-     * @param structureList The list of structures to register.
+     * @param registry
+     * @param structureList A list of structures to register.
      */
-    private static void register(Register<Structure<?>> event, List<Structure<?>> structureList)
+    private static void register(IForgeRegistry<Structure<?>> registry, List<Structure<?>> structureList)
     {
         for (Structure<?> structure : structureList)
         {
-            event.getRegistry().register(structure);
+            registry.register(structure);
             Structure.NAME_STRUCTURE_BIMAP.put(structure.getRegistryName().toString(), structure);
         }
     }
