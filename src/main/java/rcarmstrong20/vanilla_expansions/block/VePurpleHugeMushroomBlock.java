b0dbfd7b36855dbcd898f00eb3155595c620d852
rcarmstrong20 <rcarmstrong20@gmail.com>
2021-05-09 17:09:40 -0600

    Fixed the bounce motion after it broke from updating.

--- src/main/java/rcarmstrong20/vanilla_expansions/block/VePurpleHugeMushroomBlock.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/block/VePurpleHugeMushroomBlock.java
@@ -6,6 +6,8 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import rcarmstrong20.vanilla_expansions.core.VeSoundEvents;
@@ -17,29 +19,40 @@ public class VePurpleHugeMushroomBlock extends HugeMushroomBlock
         super(properties);
     }
 
-    /**
-     * Block's chance to react to a living entity falling on it.
-     */
     @Override
     public void fallOn(World world, BlockPos pos, Entity entityIn, float fallDistance)
     {
-
-        float strength = 2.0F;
-        float height = entityIn.fallDistance * strength;
-        if (height > 0 && !entityIn.isCrouching())
+        float strength = 0.5F;
+        float height = fallDistance * strength;
+        if (entityIn.isSuppressingBounce())
         {
+            Vector3d vector3d = entityIn.getDeltaMovement();
+
+            // Added a little bounce at the end and play a higher pitched bounce sound.
+            entityIn.setDeltaMovement(vector3d.x, vector3d.y + Math.sqrt(0.22 * (height + 0.25F)), vector3d.z);
+            world.playSound(null, pos, VeSoundEvents.blockMushroomBounce, SoundCategory.BLOCKS, 1.0F,
+                    0.8F + world.random.nextFloat() / 0.4F);
+        }
+        else
+        {
+            Vector3d vector3d = entityIn.getDeltaMovement();
+
+            // Set a maximum height of 100.
             if (height >= 100)
             {
                 height = 100;
             }
 
-            entityIn.setDeltaMovement(entityIn.getDeltaMovement().multiply(1.0, 0.0, 1.0));
-            entityIn.lerpMotion(0, Math.sqrt(0.22 * (height + 0.25F)), 0);
+            // Update the entities motion and play the bounce sound.
+            entityIn.setDeltaMovement(vector3d.x, -vector3d.y + Math.sqrt(0.22 * (height + 0.25F)), vector3d.z);
             world.playSound(null, pos, VeSoundEvents.blockMushroomBounce, SoundCategory.BLOCKS, 1.0F,
                     0.8F + world.random.nextFloat() * 0.4F);
         }
-        entityIn.fallDistance = 0;
     }
+
+    @Override
+    public void updateEntityAfterFallOn(IBlockReader world, Entity entityIn)
+    {}
 
     @Override
     public boolean addLandingEffects(BlockState state1, ServerWorld worldserver, BlockPos pos, BlockState state2,
