7d145f48058dfd49fbcd97928452194a748b44a5
rcarmstrong20 <rcarmstrong20@gmail.com>
2021-05-14 21:28:02 -0600

    Added a new effect tooltip.

--- src/main/java/rcarmstrong20/vanilla_expansions/item/VeTotemOfTheFortunateItem.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/item/VeTotemOfTheFortunateItem.java
@@ -1,5 +1,10 @@
 package rcarmstrong20.vanilla_expansions.item;
 
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
@@ -9,8 +14,12 @@ import net.minecraft.potion.Effects;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import rcarmstrong20.vanilla_expansions.core.VeParticleTypes;
+import rcarmstrong20.vanilla_expansions.util.VeEffectUtil;
 
 public class VeTotemOfTheFortunateItem extends Item
 {
@@ -25,7 +34,7 @@ public class VeTotemOfTheFortunateItem extends Item
     @Override
     public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand)
     {
-        player.addEffect(new EffectInstance(Effects.LUCK, 1400, amplifier));
+        player.addEffect(new EffectInstance(Effects.LUCK, 2410, amplifier)); // 2410 is a 2:00 duration
         player.playSound(SoundEvents.TOTEM_USE, 20000, 10000);
         ItemStack stack = player.getItemInHand(hand);
         int max = random.nextInt(15) + 15;
@@ -50,7 +59,17 @@ public class VeTotemOfTheFortunateItem extends Item
                         true, x, y, z, count, 0.0, 1.0, 0.0, 0.0);
             }
         }
-
         return ActionResult.success(stack);
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void appendHoverText(ItemStack stack, World world, List<ITextComponent> text, ITooltipFlag flag)
+    {
+        super.appendHoverText(stack, world, text, flag);
+
+        EffectInstance luckInstance = new EffectInstance(Effects.LUCK, 2410, amplifier); // 2410 is a 2:00 duration
+
+        VeEffectUtil.addTotemEffectTooltip(ImmutableList.of(luckInstance), text);
     }
 }
