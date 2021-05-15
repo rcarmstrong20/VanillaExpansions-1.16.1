eaf0d88f3744aabc5f06effabf593ada5f43fda6
rcarmstrong20 <rcarmstrong20@gmail.com>
2021-05-14 21:28:50 -0600

    The transmutation table now shares it's shape with the enchantment table.

--- src/main/java/rcarmstrong20/vanilla_expansions/block/VeTransmutationTableBlock.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/block/VeTransmutationTableBlock.java
@@ -12,13 +12,17 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.IWorldPosCallable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import rcarmstrong20.vanilla_expansions.inventory.container.VeTransmutationTableContainer;
 
 public class VeTransmutationTableBlock extends Block
 {
     private static final TranslationTextComponent NAME = new TranslationTextComponent("container.transmutation_table");
+    protected static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 12.0D, 16.0D);
 
     public VeTransmutationTableBlock(Properties properties)
     {
@@ -48,5 +52,11 @@ public class VeTransmutationTableBlock extends Block
             player.openMenu(state.getMenuProvider(world, pos));
             return ActionResultType.CONSUME;
         }
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader reader, BlockPos pos, ISelectionContext context)
+    {
+        return SHAPE;
     }
 }
