9527fa5352bfedad5e14496138e887aad357281b
rcarmstrong20 <rcarmstrong20@gmail.com>
2021-05-14 21:31:21 -0600

    Added a new block class for the essence extractor.

---
+++ src/main/java/rcarmstrong20/vanilla_expansions/block/VeEssenceExtractorBlock.java
@@ -0,0 +1,11 @@
+package rcarmstrong20.vanilla_expansions.block;
+
+import net.minecraft.block.Block;
+
+public class VeEssenceExtractorBlock extends Block
+{
+    public VeEssenceExtractorBlock(Properties properties)
+    {
+        super(properties);
+    }
+}
