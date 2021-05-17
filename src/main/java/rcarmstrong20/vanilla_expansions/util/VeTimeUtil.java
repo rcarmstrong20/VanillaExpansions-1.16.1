f08cf7434ccb05992b2204a15ca33d4520d61b61
rcarmstrong20 <rcarmstrong20@gmail.com>
2021-05-17 12:43:30 -0600

    Added a time utility class.

---
+++ src/main/java/rcarmstrong20/vanilla_expansions/util/VeTimeUtil.java
@@ -0,0 +1,15 @@
+package rcarmstrong20.vanilla_expansions.util;
+
+public class VeTimeUtil
+{
+    /**
+     * Converts seconds to Minecraft ticks.
+     *
+     * @param sec The number of seconds.
+     * @return The equivalent number of ticks.
+     */
+    public static int convertSecsToTicks(int sec)
+    {
+        return sec * 20;
+    }
+}
