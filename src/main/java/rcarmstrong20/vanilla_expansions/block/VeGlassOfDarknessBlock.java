436d00affd209cdc910c312576aa33f10682ca78
rcarmstrong20 <rcarmstrong20@gmail.com>
2021-05-21 20:06:55 -0600

    Made the black flame blocks blacker.

--- src/main/java/rcarmstrong20/vanilla_expansions/block/VeGlassOfDarknessBlock.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/block/VeGlassOfDarknessBlock.java
@@ -1,14 +1,14 @@
 package rcarmstrong20.vanilla_expansions.block;
 
+import net.minecraft.block.AbstractGlassBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.GlassBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 
-public class VeGlassBlock extends GlassBlock
+public class VeGlassOfDarknessBlock extends AbstractGlassBlock
 {
-    public VeGlassBlock(Block.Properties properties)
+    public VeGlassOfDarknessBlock(Block.Properties properties)
     {
         super(properties);
     }
@@ -22,10 +22,9 @@ public class VeGlassBlock extends GlassBlock
     /**
      * Checks whether this block should let light pass through.
      */
-    /*
-     * @Override public int getOpacity(BlockState state, IBlockReader worldIn,
-     * BlockPos pos) { if (this == VeBlocks.glassOfDarkness) { return
-     * worldIn.getMaxLightLevel(); } return state.propagatesSkylightDown(worldIn,
-     * pos) ? 0 : 1; }
-     */
+    @Override
+    public int getLightBlock(BlockState state, IBlockReader world, BlockPos pos)
+    {
+        return world.getMaxLightLevel();
+    }
 }
