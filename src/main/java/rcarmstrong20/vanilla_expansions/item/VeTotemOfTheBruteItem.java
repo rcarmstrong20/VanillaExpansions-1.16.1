a442fe81f56aceb3da388942611190d0316e5de6
rcarmstrong20 <rcarmstrong20@gmail.com>
2021-05-25 13:41:21 -0600

    Added the totem's power to each of their respective classes.

--- src/main/java/rcarmstrong20/vanilla_expansions/item/VeTotemOfTheBruteItem.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/item/VeTotemOfTheBruteItem.java
@@ -5,10 +5,13 @@ import java.util.List;
 import com.google.common.collect.ImmutableList;
 
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.item.Item;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
@@ -16,28 +19,53 @@ import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import rcarmstrong20.vanilla_expansions.core.VeParticleTypes;
 import rcarmstrong20.vanilla_expansions.util.VeEffectUtil;
+import rcarmstrong20.vanilla_expansions.util.VeParticleUtil;
+import rcarmstrong20.vanilla_expansions.util.VeTimeUtil;
+import rcarmstrong20.vanilla_expansions.util.VeTotemMaps;
 
-public class VeTotemOfTheBruteItem extends Item
+public class VeTotemOfTheBruteItem extends VeTotemItem
 {
     private int amplifier;
 
     public VeTotemOfTheBruteItem(Properties properties, int amplifier)
     {
-        super(properties);
+        super(properties, VeTotemMaps.BRUTE_TOTEM_MAP);
         this.amplifier = amplifier;
+    }
+
+    @Override
+    public ActionResult<ItemStack> use(World level, PlayerEntity player, Hand hand)
+    {
+        ItemStack stack = player.getItemInHand(hand);
+        int effectTicks = VeTimeUtil.convertSecsToTicks(30);
+
+        player.addEffect(new EffectInstance(Effects.DAMAGE_BOOST, effectTicks, amplifier));
+        player.addEffect(new EffectInstance(Effects.DAMAGE_RESISTANCE, effectTicks, 1));
+
+        if (!player.isCreative())
+        {
+            stack.shrink(1);
+        }
+
+        if (player instanceof ServerPlayerEntity)
+        {
+            VeParticleUtil.spawnTotemParticles(VeParticleTypes.totemOfTheBrute, (ServerPlayerEntity) player);
+        }
+        return ActionResult.success(stack);
     }
 
     @Override
     @OnlyIn(Dist.CLIENT)
     public void appendHoverText(ItemStack stack, World world, List<ITextComponent> text, ITooltipFlag flag)
     {
-        super.appendHoverText(stack, world, text, flag);
-
         EffectInstance strengthInstance = new EffectInstance(Effects.DAMAGE_BOOST, 600, amplifier);
         EffectInstance resistanceInstance = new EffectInstance(Effects.DAMAGE_RESISTANCE, 600, 1);
 
         VeEffectUtil.addTotemEffectTooltip(ImmutableList.of(strengthInstance, resistanceInstance), text);
+
+        super.appendHoverText(stack, world, text, flag);
 
         text.add(StringTextComponent.EMPTY);
         text.add(new TranslationTextComponent("bruteTotem.useDescLine1").withStyle(TextFormatting.DARK_PURPLE));
--- src/main/java/rcarmstrong20/vanilla_expansions/item/VeTotemOfTheGuardianItem.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/item/VeTotemOfTheGuardianItem.java
@@ -5,10 +5,14 @@ import java.util.List;
 import com.google.common.collect.ImmutableList;
 
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
@@ -16,7 +20,9 @@ import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import rcarmstrong20.vanilla_expansions.core.VeParticleTypes;
 import rcarmstrong20.vanilla_expansions.util.VeEffectUtil;
+import rcarmstrong20.vanilla_expansions.util.VeParticleUtil;
 import rcarmstrong20.vanilla_expansions.util.VeTimeUtil;
 
 public class VeTotemOfTheGuardianItem extends Item
@@ -27,6 +33,27 @@ public class VeTotemOfTheGuardianItem extends Item
     {
         super(properties);
         this.duration = duration;
+    }
+
+    @Override
+    public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand)
+    {
+        ItemStack stack = player.getItemInHand(hand);
+        int ticks = VeTimeUtil.convertSecsToTicks(duration);
+
+        player.addEffect(new EffectInstance(Effects.WATER_BREATHING, ticks));
+        player.addEffect(new EffectInstance(Effects.NIGHT_VISION, ticks));
+
+        if (!player.isCreative())
+        {
+            stack.shrink(1);
+        }
+
+        if (player instanceof ServerPlayerEntity)
+        {
+            VeParticleUtil.spawnTotemParticles(VeParticleTypes.totemOfTheGuardian, (ServerPlayerEntity) player);
+        }
+        return ActionResult.success(stack);
     }
 
     @Override
