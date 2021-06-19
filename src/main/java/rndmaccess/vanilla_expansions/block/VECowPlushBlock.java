ed2814ce95adee5b0e97bf3b0bd0cb5499ff2ef2
rndmaccess <rndmaccessgaming@gmail.com>
2021-06-18 18:47:39 -0600

    Simplified and improved each model and fixed the shapes respectively.

--- src/main/java/rndmaccess/vanilla_expansions/block/VECowPlushBlock.java
+++ src/main/java/rndmaccess/vanilla_expansions/block/VECowPlushBlock.java
@@ -12,35 +12,33 @@ import rndmaccess.vanilla_expansions.block.util.VEBoxBlockUtil;
 
 public class VECowPlushBlock extends VEPlushBlock
 {
-    protected static final VoxelShape COW_NORTH_TORSO_SHAPE = Block.box(4.5D, 4.0D, 4.0D, 11.5D, 10.0D, 14.5D);
-    protected static final VoxelShape COW_NORTH_HEAD_SHAPE = Block.box(5.5D, 6.0D, 1.0D, 10.5D, 11.0D, 4.0D);
-    protected static final VoxelShape COW_NORTH_UTTERS_SHAPE = Block.box(7.0D, 3.0D, 11.0D, 9.0D, 5.5D, 15.0D);
-    protected static final VoxelShape COW_NORTH_MOUTH_SHAPE = Block.box(6.5D, 6.5D, 0.5D, 9.5D, 9.0D, 1.5D);
-    protected static final VoxelShape COW_NORTH_RIGHT_EYE_SHAPE = Block.box(9.0D, 9.5D, 0.5D, 10.0D, 10.5D, 1.5D);
-    protected static final VoxelShape COW_NORTH_LEFT_EYE_SHAPE = Block.box(6.0D, 9.5D, 0.5D, 7.0D, 10.5D, 1.5D);
-    protected static final VoxelShape COW_NORTH_RIGHT_HORN_SHAPE = Block.box(10.5D, 10.0D, 1.5D, 11.0D, 11.5D, 2.5D);
-    protected static final VoxelShape COW_NORTH_LEFT_HORN_SHAPE = Block.box(5.0D, 10.0D, 1.5D, 5.5D, 11.5D, 2.5D);
-    protected static final VoxelShape COW_NORTH_LEFT_FRONT_LEG_SHAPE = Block.box(5.0D, 0.0D, 5.0D, 7.0D, 4.0D, 7.0D);
-    protected static final VoxelShape COW_NORTH_RIGHT_FRONT_LEG_SHAPE = Block.box(9.0D, 0.0D, 5.0D, 11.0D, 4.0D, 7.0D);
-    protected static final VoxelShape COW_NORTH_RIGHT_BACK_LEG_SHAPE = Block.box(9.0D, 0.0D, 12.0D, 11.0D, 4.0D, 14.0D);
-    protected static final VoxelShape COW_NORTH_LEFT_BACK_LEG_SHAPE = Block.box(5.0D, 0.0D, 12.0D, 7.0D, 4.0D, 14.0D);
+    protected static final VoxelShape NORTH_TORSO_SHAPE = Block.box(3.5D, 2.0D, 6.0D, 12.5D, 8.0D, 14.5D);
+    protected static final VoxelShape NORTH_HEAD_SHAPE = Block.box(4.0D, 5.5D, 3.0D, 12.0D, 13.0D, 10.5D);
+    protected static final VoxelShape NORTH_SNOUT_SHAPE = Block.box(6.0D, 6.0D, 1.5D, 10.0D, 9.0D, 3.5D);
+    protected static final VoxelShape NORTH_UTTER_SHAPE = Block.box(7.0D, 1.0D, 11.5D, 9.0D, 3.0D, 15.0D);
 
-    protected static final VoxelShape COW_NORTH_LEGS_SHAPE = VoxelShapes.or(COW_NORTH_LEFT_FRONT_LEG_SHAPE,
-            COW_NORTH_RIGHT_FRONT_LEG_SHAPE, COW_NORTH_RIGHT_BACK_LEG_SHAPE, COW_NORTH_LEFT_BACK_LEG_SHAPE);
+    protected static final VoxelShape NORTH_BACK_LEFT_LEG_SHAPE = Block.box(10.5D, 0.0D, 12.5D, 12.5D, 2.0D, 14.5D);
+    protected static final VoxelShape NORTH_BACK_RIGHT_LEG_SHAPE = Block.box(3.5D, 0.0D, 12.5D, 5.5D, 2.0D, 14.5D);
+    protected static final VoxelShape NORTH_FRONT_RIGHT_LEG_SHAPE = Block.box(3.5D, 0.0D, 6.0D, 5.5D, 2.0D, 8.0D);
+    protected static final VoxelShape NORTH_FRONT_LEFT_LEG_SHAPE = Block.box(10.5D, 0.0D, 6.0D, 12.5D, 2.0D, 8.0D);
 
-    protected static final VoxelShape COW_NORTH_HORNS_SHAPE = VoxelShapes.or(COW_NORTH_RIGHT_HORN_SHAPE,
-            COW_NORTH_LEFT_HORN_SHAPE);
+    protected static final VoxelShape NORTH_LEFT_EYE_SHAPE = Block.box(9.0D, 9.5D, 2.5D, 11.0D, 11.5D, 3.0D);
+    protected static final VoxelShape NORTH_RIGHT_EYE_SHAPE = Block.box(5.0D, 9.5D, 2.5D, 7.0D, 11.5D, 3.0D);
 
-    protected static final VoxelShape COW_NORTH_EYES_SHAPE = VoxelShapes.or(COW_NORTH_RIGHT_EYE_SHAPE,
-            COW_NORTH_LEFT_EYE_SHAPE);
+    protected static final VoxelShape NORTH_RIGHT_HORN_SHAPE = Block.box(2.5D, 10.5D, 5.0D, 5.0D, 14.0D, 8.0D);
+    protected static final VoxelShape NORTH_LEFT_HORN_SHAPE = Block.box(11.0D, 10.5D, 5.0D, 13.5D, 14.0D, 8.0D);
 
-    protected static final VoxelShape COW_NORTH_SHAPE = VoxelShapes.or(COW_NORTH_TORSO_SHAPE, COW_NORTH_HEAD_SHAPE,
-            COW_NORTH_UTTERS_SHAPE, COW_NORTH_MOUTH_SHAPE, COW_NORTH_LEGS_SHAPE, COW_NORTH_HORNS_SHAPE,
-            COW_NORTH_EYES_SHAPE);
+    protected static final VoxelShape NORTH_LEGS_SHAPE = VoxelShapes.or(NORTH_BACK_LEFT_LEG_SHAPE,
+            NORTH_BACK_RIGHT_LEG_SHAPE, NORTH_FRONT_RIGHT_LEG_SHAPE, NORTH_FRONT_LEFT_LEG_SHAPE);
+    protected static final VoxelShape NORTH_EYES_SHAPE = VoxelShapes.or(NORTH_LEFT_EYE_SHAPE, NORTH_RIGHT_EYE_SHAPE);
+    protected static final VoxelShape NORTH_HORNS_SHAPE = VoxelShapes.or(NORTH_RIGHT_HORN_SHAPE, NORTH_LEFT_HORN_SHAPE);
 
-    protected static final VoxelShape COW_SOUTH_SHAPE = VEBoxBlockUtil.rotate180(Axis.Y, COW_NORTH_SHAPE);
-    protected static final VoxelShape COW_WEST_SHAPE = VEBoxBlockUtil.rotate270(Axis.Y, COW_NORTH_SHAPE);
-    protected static final VoxelShape COW_EAST_SHAPE = VEBoxBlockUtil.rotate90(Axis.Y, COW_NORTH_SHAPE);
+    protected static final VoxelShape NORTH_SHAPE = VoxelShapes.or(NORTH_TORSO_SHAPE, NORTH_LEGS_SHAPE,
+            NORTH_HEAD_SHAPE, NORTH_SNOUT_SHAPE, NORTH_EYES_SHAPE, NORTH_HORNS_SHAPE, NORTH_UTTER_SHAPE);
+
+    protected static final VoxelShape SOUTH_SHAPE = VEBoxBlockUtil.rotate180(Axis.Y, NORTH_SHAPE);
+    protected static final VoxelShape WEST_SHAPE = VEBoxBlockUtil.rotate270(Axis.Y, NORTH_SHAPE);
+    protected static final VoxelShape EAST_SHAPE = VEBoxBlockUtil.rotate90(Axis.Y, NORTH_SHAPE);
 
     public VECowPlushBlock(Properties properties)
     {
@@ -50,6 +48,6 @@ public class VECowPlushBlock extends VEPlushBlock
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context)
     {
-        return defineShapes(state, COW_NORTH_SHAPE, COW_SOUTH_SHAPE, COW_WEST_SHAPE, COW_EAST_SHAPE);
+        return defineShapes(state, NORTH_SHAPE, SOUTH_SHAPE, WEST_SHAPE, EAST_SHAPE);
     }
 }
--- src/main/java/rndmaccess/vanilla_expansions/block/VEPigPlushBlock.java
+++ src/main/java/rndmaccess/vanilla_expansions/block/VEPigPlushBlock.java
@@ -12,27 +12,32 @@ import rndmaccess.vanilla_expansions.block.util.VEBoxBlockUtil;
 
 public class VEPigPlushBlock extends VEPlushBlock
 {
-    protected static final VoxelShape NORTH_BODY = Block.box(4.5D, 4.0D, 4.0D, 11.5D, 9.0D, 15.0D);
-    protected static final VoxelShape NORTH_HEAD = Block.box(5.5D, 5.0D, 1.0D, 10.5D, 10.0D, 5.0D);
-    protected static final VoxelShape NORTH_FRONT_RIGHT_LEG = Block.box(9.0D, 0.0D, 6.0D, 11.5D, 4.0D, 8.0D);
-    protected static final VoxelShape NORTH_FRONT_LEFT_LEG = Block.box(4.5D, 0.0D, 6.0D, 7.0D, 4.0D, 8.0D);
-    protected static final VoxelShape NORTH_BACK_RIGHT_LEG = Block.box(9.0D, 0.0D, 13.5D, 11.5D, 4.0D, 15.5D);
-    protected static final VoxelShape NORTH_BACK_LEFT_LEG = Block.box(4.5D, 0.0D, 13.5D, 7.0D, 4.0D, 15.5D);
-    protected static final VoxelShape NORTH_TAIL1 = Block.box(9.0D, 4.0D, 15.0D, 10.0D, 7.5D, 15.5D);
-    protected static final VoxelShape NORTH_TAIL2 = Block.box(6.0D, 4.0D, 15.0D, 9.0D, 5.0D, 15.5D);
-    protected static final VoxelShape NORTH_TAIL3 = Block.box(7.0D, 7.5D, 15.0D, 9.0D, 8.5D, 15.5D);
-    protected static final VoxelShape NORTH_TAIL4 = Block.box(6.0D, 6.5D, 15.0D, 7.0D, 7.5D, 15.5D);
-    protected static final VoxelShape NORTH_TAIL5 = Block.box(7.0D, 5.5D, 15.0D, 8.0D, 6.5D, 15.5D);
-    protected static final VoxelShape NORTH_RIGHT_EYE = Block.box(8.5D, 7.5D, 0.5D, 10.0D, 8.5D, 1.0D);
-    protected static final VoxelShape NORTH_LEFT_EYE = Block.box(6.0D, 7.5D, 0.5D, 7.5D, 8.5D, 1.0D);
-    protected static final VoxelShape NORTH_SNOUT = Block.box(7.0D, 6.0D, 0.0D, 9.0D, 7.0D, 1.0D);
-    protected static final VoxelShape NORTH_LEGS = VoxelShapes.or(NORTH_FRONT_RIGHT_LEG, NORTH_FRONT_LEFT_LEG,
-            NORTH_BACK_RIGHT_LEG, NORTH_BACK_LEFT_LEG);
-    protected static final VoxelShape NORTH_TAIL = VoxelShapes.or(NORTH_TAIL1, NORTH_TAIL2, NORTH_TAIL3, NORTH_TAIL4,
-            NORTH_TAIL5);
-    protected static final VoxelShape NORTH_EYES = VoxelShapes.or(NORTH_RIGHT_EYE, NORTH_LEFT_EYE);
-    protected static final VoxelShape NORTH_SHAPE = VoxelShapes.or(NORTH_BODY, NORTH_HEAD, NORTH_LEGS, NORTH_TAIL,
-            NORTH_EYES, NORTH_SNOUT);
+    protected static final VoxelShape NORTH_BODY = Block.box(4.0D, 3.0D, 4.0D, 12.0D, 9.0D, 14.0D);
+    protected static final VoxelShape NORTH_HEAD = Block.box(4.5D, 6.0D, 2.0D, 11.5D, 12.0D, 7.5D);
+    protected static final VoxelShape NORTH_SNOUT = Block.box(6.5D, 6.5D, 1.0D, 9.5D, 8.5D, 2.0D);
+
+    protected static final VoxelShape NORTH_LEFT_EYE = Block.box(8.5D, 9.0D, 1.5D, 10.5D, 11.0D, 2.0D);
+    protected static final VoxelShape NORTH_RIGHT_EYE = Block.box(5.5D, 9.0D, 1.5D, 7.5D, 11.0D, 2.0D);
+
+    protected static final VoxelShape NORTH_LEFT_BACK_LEG = Block.box(10.0D, 0.0D, 12.0D, 12.0D, 3.0D, 14.0D);
+    protected static final VoxelShape NORTH_RIGHT_BACK_LEG = Block.box(4.0D, 0.0D, 12.0D, 6.0D, 3.0D, 14.0D);
+    protected static final VoxelShape NORTH_LEFT_FRONT_LEG = Block.box(10.0D, 0.0D, 4.0D, 12.0D, 3.0D, 6.0D);
+    protected static final VoxelShape NORTH_RIGHT_FRONT_LEG = Block.box(4.0D, 0.0D, 4.0D, 6.0D, 3.0D, 6.0D);
+
+    protected static final VoxelShape NORTH_TAIL_BOTTOM = Block.box(6.5D, 3.5D, 14.0D, 10.0D, 4.5D, 15.0D);
+    protected static final VoxelShape NORTH_TAIL_SIDE = Block.box(10.0D, 4.5D, 14.0D, 11.0D, 7.5D, 15.0D);
+    protected static final VoxelShape NORTH_TAIL_TOP = Block.box(7.0D, 7.5D, 14.0D, 10.0D, 8.5D, 15.0D);
+    protected static final VoxelShape NORTH_TAIL_TWIRL_START = Block.box(6.0D, 6.5D, 14.0D, 7.0D, 7.5D, 15.0D);
+    protected static final VoxelShape NORTH_TAIL_TWIRL_END = Block.box(7.0D, 5.5D, 14.0D, 8.0D, 6.5D, 15.0D);
+
+    protected static final VoxelShape NORTH_EYES = VoxelShapes.or(NORTH_LEFT_EYE, NORTH_RIGHT_EYE);
+    protected static final VoxelShape NORTH_LEGS = VoxelShapes.or(NORTH_LEFT_BACK_LEG, NORTH_RIGHT_BACK_LEG,
+            NORTH_LEFT_FRONT_LEG, NORTH_RIGHT_FRONT_LEG);
+    protected static final VoxelShape NORTH_TAIL = VoxelShapes.or(NORTH_TAIL_BOTTOM, NORTH_TAIL_SIDE, NORTH_TAIL_TOP,
+            NORTH_TAIL_TWIRL_START, NORTH_TAIL_TWIRL_END);
+    protected static final VoxelShape NORTH_SHAPE = VoxelShapes.or(NORTH_BODY, NORTH_HEAD, NORTH_SNOUT, NORTH_EYES,
+            NORTH_LEGS, NORTH_TAIL);
+
     protected static final VoxelShape SOUTH_SHAPE = VEBoxBlockUtil.rotate180(Axis.Y, NORTH_SHAPE);
     protected static final VoxelShape WEST_SHAPE = VEBoxBlockUtil.rotate270(Axis.Y, NORTH_SHAPE);
     protected static final VoxelShape EAST_SHAPE = VEBoxBlockUtil.rotate90(Axis.Y, NORTH_SHAPE);
