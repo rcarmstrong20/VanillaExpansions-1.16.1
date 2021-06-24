5be2a6ddf305f134ca71991bb3c05b8a11dd80f8
rndmaccess <rndmaccessgaming@gmail.com>
2021-06-23 19:39:10 -0600

    Improved the sheep plush model.

--- src/main/java/rndmaccess/vanilla_expansions/block/VESheepPlushBlock.java
+++ src/main/java/rndmaccess/vanilla_expansions/block/VESheepPlushBlock.java
@@ -12,41 +12,34 @@ import rndmaccess.vanilla_expansions.block.util.VEBoxBlockUtil;
 
 public class VESheepPlushBlock extends VEPlushBlock
 {
-    protected static final VoxelShape SHEEP_NORTH_BACK_RIGHT_LEG_TUFF_SHAPE = Block.box(8.5D, 2.0D, 3.5D, 11.0D, 4.0D,
-            6.0D);
-    protected static final VoxelShape SHEEP_NORTH_BACK_RIGHT_LEG_SHAPE = Block.box(9.0D, 0.0D, 4.0D, 10.5D, 2.0D, 5.5D);
-    protected static final VoxelShape SHEEP_NORTH_BACK_LEFT_LEG_TUFF_SHAPE = Block.box(5.0D, 2.0D, 3.5D, 7.5D, 4.0D,
-            6.0D);
-    protected static final VoxelShape SHEEP_NORTH_BACK_LEFT_LEG_SHAPE = Block.box(5.5D, 0.0D, 4.0D, 7.0D, 2.0D, 5.5D);
-    protected static final VoxelShape SHEEP_NORTH_FRONT_RIGHT_LEG_TUFF_SHAPE = Block.box(8.5D, 2.0D, 10.0D, 11.0D, 4.0D,
-            12.5D);
-    protected static final VoxelShape SHEEP_NORTH_FRONT_RIGHT_LEG_SHAPE = Block.box(9.0D, 0.0D, 10.5D, 10.5D, 2.0D,
-            12.0D);
-    protected static final VoxelShape SHEEP_NORTH_FRONT_LEFT_LEG_TUFF_SHAPE = Block.box(5.0D, 2.0D, 10.0D, 7.5D, 4.0D,
-            12.5D);
-    protected static final VoxelShape SHEEP_NORTH_FRONT_LEFT_LEG_SHAPE = Block.box(5.5D, 0.0D, 10.5D, 7.0D, 2.0D,
-            12.0D);
-    protected static final VoxelShape SHEEP_NORTH_HEAD_SHAPE = Block.box(5.5D, 9.0D, 1.0D, 10.5D, 14.0D, 6.0D);
-    protected static final VoxelShape SHEEP_NORTH_TORSO_SHAPE = Block.box(4.5D, 4.0D, 3.0D, 11.5D, 11.0D, 14.0D);
-    protected static final VoxelShape SHEEP_NORTH_FOREHEAD_SHAPE = Block.box(6.0D, 9.5D, 0.5D, 10.0D, 13.5D, 1.0D);
-    protected static final VoxelShape SHEEP_NORTH_EARS_SHAPE = Block.box(5.5D, 11.0D, 0.5D, 10.5D, 12.5D, 1.0D);
-    protected static final VoxelShape SHEEP_NORTH_RIGHT_EYE_SHAPE = Block.box(8.5D, 12.0D, 0.5D, 10.0D, 11.0D, 0.0D);
-    protected static final VoxelShape SHEEP_NORTH_LEFT_EYE_SHAPE = Block.box(6.0D, 12.0D, 0.5D, 7.5D, 11.0D, 0.0D);
-    protected static final VoxelShape SHEEP_NORTH_MOUTH_SHAPE = Block.box(7.5D, 9.5D, 0.5D, 8.5D, 10.5D, 0.0D);
+    protected static final VoxelShape NORTH_TORSO = Block.box(4.0D, 4.0D, 4.5D, 12.0D, 9.0D, 14.0D);
+    protected static final VoxelShape NORTH_HEAD = Block.box(4.5D, 5.0D, 2.0D, 11.5D, 12.0D, 8.0D);
 
-    protected static final VoxelShape SHEEP_NORTH_LEG_TUFFS_SHAPE = VoxelShapes.or(
-            SHEEP_NORTH_BACK_RIGHT_LEG_TUFF_SHAPE, SHEEP_NORTH_BACK_LEFT_LEG_TUFF_SHAPE,
-            SHEEP_NORTH_FRONT_RIGHT_LEG_TUFF_SHAPE, SHEEP_NORTH_FRONT_LEFT_LEG_TUFF_SHAPE);
+    protected static final VoxelShape NORTH_FRONT_RIGHT_LEG_TUFF = Block.box(4.0D, 2.0D, 4.5D, 7.0D, 4.0D, 7.5D);
+    protected static final VoxelShape NORTH_FRONT_RIGHT_LEG = Block.box(4.5D, 0.0D, 5.0D, 6.5D, 2.0D, 7.0D);
 
-    protected static final VoxelShape SHEEP_NORTH_LEGS_SHAPE = VoxelShapes.or(SHEEP_NORTH_BACK_RIGHT_LEG_SHAPE,
-            SHEEP_NORTH_BACK_LEFT_LEG_SHAPE, SHEEP_NORTH_FRONT_RIGHT_LEG_SHAPE, SHEEP_NORTH_FRONT_LEFT_LEG_SHAPE);
+    protected static final VoxelShape NORTH_BACK_LEFT_LEG_TUFF = Block.box(9.0D, 2.0D, 11.0D, 12.0D, 4.0D, 14.0D);
+    protected static final VoxelShape NORTH_BACK_LEFT_LEG = Block.box(9.5D, 0.0D, 11.5D, 11.5D, 2.0D, 13.5D);
 
-    protected static final VoxelShape SHEEP_NORTH_EYES_SHAPE = VoxelShapes.or(SHEEP_NORTH_RIGHT_EYE_SHAPE,
-            SHEEP_NORTH_LEFT_EYE_SHAPE);
+    protected static final VoxelShape NORTH_FRONT_LEFT_LEG_TUFF = Block.box(9.0D, 2.0D, 4.5D, 12.0D, 4.0D, 7.5D);
+    protected static final VoxelShape NORTH_FRONT_LEFT_LEG = Block.box(9.5D, 0.0D, 5.0D, 11.5D, 2.0D, 7.0D);
 
-    protected static final VoxelShape SHEEP_NORTH_SHAPE = VoxelShapes.or(SHEEP_NORTH_HEAD_SHAPE,
-            SHEEP_NORTH_TORSO_SHAPE, SHEEP_NORTH_FOREHEAD_SHAPE, SHEEP_NORTH_EARS_SHAPE, SHEEP_NORTH_MOUTH_SHAPE,
-            SHEEP_NORTH_LEG_TUFFS_SHAPE, SHEEP_NORTH_LEGS_SHAPE, SHEEP_NORTH_EYES_SHAPE);
+    protected static final VoxelShape NORTH_BACK_RIGHT_LEG_TUFF = Block.box(4.0D, 2.0D, 11.0D, 7.0D, 4.0D, 14.0D);
+    protected static final VoxelShape NORTH_BACK_RIGHT_LEG = Block.box(4.5D, 0.0D, 11.5D, 6.5D, 2.0D, 13.5D);
+
+    protected static final VoxelShape NORTH_LEFT_EYE = Block.box(8.5D, 8.0D, 1.5D, 10.5D, 9.0D, 2.0D);
+    protected static final VoxelShape NORTH_RIGHT_EYE = Block.box(5.5D, 8.0D, 1.5D, 7.5D, 9.0D, 2.0D);
+
+    protected static final VoxelShape NORTH_MOUTH = Block.box(7.5D, 6.0D, 1.5D, 8.5D, 7.0D, 2.0D);
+
+    protected static final VoxelShape NORTH_LEG_TUFFS = VoxelShapes.or(NORTH_FRONT_RIGHT_LEG_TUFF,
+            NORTH_BACK_LEFT_LEG_TUFF, NORTH_FRONT_LEFT_LEG_TUFF, NORTH_BACK_RIGHT_LEG_TUFF);
+    protected static final VoxelShape NORTH_LEGS = VoxelShapes.or(NORTH_FRONT_RIGHT_LEG, NORTH_BACK_LEFT_LEG,
+            NORTH_FRONT_LEFT_LEG, NORTH_BACK_RIGHT_LEG);
+    protected static final VoxelShape NORTH_EYES = VoxelShapes.or(NORTH_LEFT_EYE, NORTH_RIGHT_EYE);
+
+    protected static final VoxelShape SHEEP_NORTH_SHAPE = VoxelShapes.or(NORTH_TORSO, NORTH_HEAD, NORTH_MOUTH,
+            NORTH_LEG_TUFFS, NORTH_LEGS, NORTH_EYES);
 
     protected static final VoxelShape SHEEP_SOUTH_SHAPE = VEBoxBlockUtil.rotate180(Axis.Y, SHEEP_NORTH_SHAPE);
     protected static final VoxelShape SHEEP_WEST_SHAPE = VEBoxBlockUtil.rotate270(Axis.Y, SHEEP_NORTH_SHAPE);
