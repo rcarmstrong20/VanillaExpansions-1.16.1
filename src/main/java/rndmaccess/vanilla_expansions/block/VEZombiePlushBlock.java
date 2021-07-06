c145edb4ce4fccdfa5b30ac8c08fd6beda6f1367
rndmaccess <rndmaccessgaming@gmail.com>
2021-07-05 23:51:57 -0600

    Improved the zombie plush's model.

--- src/main/java/rndmaccess/vanilla_expansions/block/VEZombiePlushBlock.java
+++ src/main/java/rndmaccess/vanilla_expansions/block/VEZombiePlushBlock.java
@@ -12,34 +12,28 @@ import rndmaccess.vanilla_expansions.block.util.VEBoxBlockUtil;
 
 public class VEZombiePlushBlock extends VEPlushBlock
 {
-    protected static final VoxelShape ZOMBIE_NORTH_HEAD_SHAPE = Block.box(5.0D, 11.0D, 7.5D, 11.0D, 16.0D, 12.0D);
-    protected static final VoxelShape ZOMBIE_NORTH_TORSO_SHAPE = Block.box(5.5D, 0.0D, 8.5D, 10.5D, 11.0D, 11.5D);
-    protected static final VoxelShape ZOMBIE_NORTH_RIGHT_ARM_SHAPE = Block.box(10.5D, 9.0D, 4.0D, 12.5D, 11.0D, 11.0D);
-    protected static final VoxelShape ZOMBIE_NORTH_LEFT_ARM_SHAPE = Block.box(3.5D, 9.0D, 4.0D, 5.5D, 11.0D, 11.0D);
-    protected static final VoxelShape ZOMBIE_NORTH_RIGHT_EYE_SHAPE = Block.box(9.0D, 14.0D, 7.0D, 10.5D, 15.0D, 7.5D);
-    protected static final VoxelShape ZOMBIE_NORTH_LEFT_EYE_SHAPE = Block.box(5.5D, 14.0D, 7.0D, 7.0D, 15.0D, 7.5D);
-    protected static final VoxelShape ZOMBIE_NORTH_MIDDLE_MOUTH_SHAPE = Block.box(7.0D, 13.0D, 7.0D, 9.0D, 14.0D, 7.5D);
-    protected static final VoxelShape ZOMBIE_NORTH_RIGHT_MOUTH_SHAPE = Block.box(9.0D, 12.0D, 7.0D, 10.0D, 13.0D, 7.5D);
-    protected static final VoxelShape ZOMBIE_NORTH_LEFT_MOUTH_SHAPE = Block.box(6.0D, 11.0D, 7.0D, 7.0D, 13.0D, 7.5D);
+    protected static final VoxelShape NORTH_HEAD = Block.box(3.5D, 7.0D, 5.0D, 12.5D, 15.0D, 13.5D);
+    protected static final VoxelShape NORTH_TORSO = Block.box(4.0D, 2.0D, 7.0D, 12.0D, 7.0D, 12.0D);
 
-    protected static final VoxelShape ZOMBIE_NORTH_ARMS_SHAPE = VoxelShapes.or(ZOMBIE_NORTH_RIGHT_ARM_SHAPE,
-            ZOMBIE_NORTH_LEFT_ARM_SHAPE);
+    protected static final VoxelShape NORTH_LEFT_LEG = Block.box(8.5D, 0.0D, 8.0D, 11.5D, 2.0D, 11.0D);
+    protected static final VoxelShape NORTH_RIGHT_LEG = Block.box(4.5D, 0.0D, 8.0D, 7.5D, 2.0D, 11.0D);
 
-    protected static final VoxelShape ZOMBIE_NORTH_EYES_SHAPE = VoxelShapes.or(ZOMBIE_NORTH_RIGHT_EYE_SHAPE,
-            ZOMBIE_NORTH_LEFT_EYE_SHAPE);
+    protected static final VoxelShape NORTH_LEFT_ARM = Block.box(11.0D, 5.0D, 3.0D, 14.0D, 8.0D, 10.5D);
+    protected static final VoxelShape NORTH_RIGHT_ARM = Block.box(2.0D, 5.0D, 3.0D, 5.0D, 8.0D, 10.5D);
 
-    protected static final VoxelShape ZOMBIE_NORTH_MOUTH_SHAPE = VoxelShapes.or(ZOMBIE_NORTH_MIDDLE_MOUTH_SHAPE,
-            ZOMBIE_NORTH_RIGHT_MOUTH_SHAPE, ZOMBIE_NORTH_LEFT_MOUTH_SHAPE);
+    protected static final VoxelShape NORTH_LEFT_EYE = Block.box(9.0D, 11.0D, 4.5D, 11.0D, 13.0D, 5.0D);
+    protected static final VoxelShape NORTH_RIGHT_EYE = Block.box(5.0D, 11.0D, 4.5D, 7.0D, 13.0D, 5.0D);
 
-    protected static final VoxelShape ZOMBIE_NORTH_BODY_SHAPE = VoxelShapes.or(ZOMBIE_NORTH_HEAD_SHAPE,
-            ZOMBIE_NORTH_TORSO_SHAPE, ZOMBIE_NORTH_ARMS_SHAPE);
+    protected static final VoxelShape NORTH_LEGS = VoxelShapes.or(NORTH_LEFT_LEG, NORTH_RIGHT_LEG);
+    protected static final VoxelShape NORTH_ARMS = VoxelShapes.or(NORTH_LEFT_ARM, NORTH_RIGHT_ARM);
+    protected static final VoxelShape NORTH_EYES = VoxelShapes.or(NORTH_LEFT_EYE, NORTH_RIGHT_EYE);
 
-    protected static final VoxelShape ZOMBIE_NORTH_SHAPE = VoxelShapes.or(ZOMBIE_NORTH_BODY_SHAPE,
-            ZOMBIE_NORTH_EYES_SHAPE, ZOMBIE_NORTH_MOUTH_SHAPE);
+    protected static final VoxelShape NORTH_SHAPE = VoxelShapes.or(NORTH_HEAD, NORTH_TORSO, NORTH_LEGS, NORTH_ARMS,
+            NORTH_EYES);
 
-    protected static final VoxelShape ZOMBIE_SOUTH_SHAPE = VEBoxBlockUtil.rotate180(Axis.Y, ZOMBIE_NORTH_SHAPE);
-    protected static final VoxelShape ZOMBIE_WEST_SHAPE = VEBoxBlockUtil.rotate270(Axis.Y, ZOMBIE_NORTH_SHAPE);
-    protected static final VoxelShape ZOMBIE_EAST_SHAPE = VEBoxBlockUtil.rotate90(Axis.Y, ZOMBIE_NORTH_SHAPE);
+    protected static final VoxelShape SOUTH_SHAPE = VEBoxBlockUtil.rotate180(Axis.Y, NORTH_SHAPE);
+    protected static final VoxelShape WEST_SHAPE = VEBoxBlockUtil.rotate270(Axis.Y, NORTH_SHAPE);
+    protected static final VoxelShape EAST_SHAPE = VEBoxBlockUtil.rotate90(Axis.Y, NORTH_SHAPE);
 
     public VEZombiePlushBlock(Properties properties)
     {
@@ -49,6 +43,6 @@ public class VEZombiePlushBlock extends VEPlushBlock
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context)
     {
-        return defineShapes(state, ZOMBIE_NORTH_SHAPE, ZOMBIE_SOUTH_SHAPE, ZOMBIE_WEST_SHAPE, ZOMBIE_EAST_SHAPE);
+        return defineShapes(state, NORTH_SHAPE, SOUTH_SHAPE, WEST_SHAPE, EAST_SHAPE);
     }
 }
