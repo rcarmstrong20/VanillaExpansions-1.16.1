d5946017dbc4cf11dc47288270262cfacdcc63f5
rndmaccess <rndmaccessgaming@gmail.com>
2021-07-12 15:59:50 -0600

    Improved the creeper plush model.

--- src/main/java/rndmaccess/vanilla_expansions/block/VECreeperPlushBlock.java
+++ src/main/java/rndmaccess/vanilla_expansions/block/VECreeperPlushBlock.java
@@ -11,32 +11,29 @@ import rndmaccess.vanilla_expansions.util.VEBoxUtil;
 
 public class VECreeperPlushBlock extends VEPlushBlock
 {
-    protected static final VoxelShape CREEPER_NORTH_HEAD_SHAPE = Block.box(4.5D, 9.0D, 4.5D, 11.5D, 16.0D, 11.5D);
-    protected static final VoxelShape CREEPER_NORTH_TORSO_SHAPE = Block.box(5.5D, 3.0D, 5.5D, 10.5D, 9.0D, 10.5D);
-    protected static final VoxelShape CREEPER_NORTH_FRONT_FOOT_SHAPE = Block.box(5.5D, 0.0D, 2.5D, 10.5D, 3.0D, 5.5D);
-    protected static final VoxelShape CREEPER_NORTH_BACK_FOOT_SHAPE = Block.box(5.5D, 0.0D, 10.5D, 10.5D, 3.0D, 13.5D);
-    protected static final VoxelShape CREEPER_NORTH_RIGHT_EYE_SHAPE = Block.box(8.5D, 12.5D, 4.0D, 10.5D, 14.5D, 4.5D);
-    protected static final VoxelShape CREEPER_NORTH_LEFT_EYE_SHAPE = Block.box(5.5D, 12.5D, 4.0D, 7.5D, 14.5D, 4.5D);
-    protected static final VoxelShape CREEPER_NORTH_MIDDLE_MOUTH_SHAPE = Block.box(7.5D, 10.5D, 4.0D, 8.5D, 12.5D,
-            4.5D);
-    protected static final VoxelShape CREEPER_NORTH_RIGHT_MOUTH_SHAPE = Block.box(8.5D, 9.5D, 4.0D, 9.5D, 11.5D, 4.5D);
-    protected static final VoxelShape CREEPER_NORTH_LEFT_MOUTH_SHAPE = Block.box(6.5D, 9.5D, 4.0D, 7.5D, 11.5D, 4.5D);
+    protected static final VoxelShape NORTH_HEAD = Block.box(4.0, 7.0, 4.0D, 12.0D, 15.0D, 12.0D);
+    protected static final VoxelShape NORTH_TORSO = Block.box(6.0D, 3.0D, 6.0D, 10.0D, 7.0D, 10.0D);
 
-    protected static final VoxelShape CREEPER_NORTH_FEET_SHAPE = VoxelShapes.or(CREEPER_NORTH_FRONT_FOOT_SHAPE,
-            CREEPER_NORTH_BACK_FOOT_SHAPE);
+    protected static final VoxelShape NORTH_BACK_FOOT = Block.box(6.0D, 0.0D, 10.0D, 10.0D, 3.0D, 13.0D);
+    protected static final VoxelShape NORTH_FRONT_FOOT = Block.box(6.0D, 0.0D, 3.0D, 10.0D, 3.0D, 6.0D);
 
-    protected static final VoxelShape CREEPER_NORTH_EYES_SHAPE = VoxelShapes.or(CREEPER_NORTH_RIGHT_EYE_SHAPE,
-            CREEPER_NORTH_LEFT_EYE_SHAPE);
+    protected static final VoxelShape NORTH_LEFT_EYE = Block.box(9.0D, 11.0D, 3.5D, 11.0D, 13.0D, 4.0D);
+    protected static final VoxelShape NORTH_RIGHT_EYE = Block.box(5.0D, 11.0D, 3.5D, 7.0D, 13.0D, 4.0D);
 
-    protected static final VoxelShape CREEPER_NORTH_MOUTH_SHAPE = VoxelShapes.or(CREEPER_NORTH_MIDDLE_MOUTH_SHAPE,
-            CREEPER_NORTH_RIGHT_MOUTH_SHAPE, CREEPER_NORTH_LEFT_MOUTH_SHAPE);
+    protected static final VoxelShape NORTH_MIDDLE_MOUTH = Block.box(7.5D, 9.0D, 3.5D, 8.5D, 11.0D, 4.0D);
+    protected static final VoxelShape NORTH_RIGHT_MOUTH = Block.box(6.5D, 8.0D, 3.5D, 7.5D, 10.0D, 4.0D);
+    protected static final VoxelShape NORTH_LEFT_MOUTH = Block.box(8.5D, 8.0D, 3.5D, 9.5D, 10.0D, 4.0D);
 
-    protected static final VoxelShape CREEPER_NORTH_SHAPE = VoxelShapes.or(CREEPER_NORTH_HEAD_SHAPE,
-            CREEPER_NORTH_TORSO_SHAPE, CREEPER_NORTH_FEET_SHAPE, CREEPER_NORTH_EYES_SHAPE, CREEPER_NORTH_MOUTH_SHAPE);
+    protected static final VoxelShape NORTH_FEET = VoxelShapes.or(NORTH_BACK_FOOT, NORTH_FRONT_FOOT);
+    protected static final VoxelShape NORTH_EYES = VoxelShapes.or(NORTH_LEFT_EYE, NORTH_RIGHT_EYE);
+    protected static final VoxelShape NORTH_MOUTH = VoxelShapes.or(NORTH_MIDDLE_MOUTH, NORTH_RIGHT_MOUTH,
+            NORTH_LEFT_MOUTH);
+    protected static final VoxelShape NORTH_BODY = VoxelShapes.or(NORTH_HEAD, NORTH_TORSO);
 
-    protected static final VoxelShape CREEPER_SOUTH_SHAPE = VEBoxUtil.rotate180(CREEPER_NORTH_SHAPE);
-    protected static final VoxelShape CREEPER_WEST_SHAPE = VEBoxUtil.rotate270(CREEPER_NORTH_SHAPE);
-    protected static final VoxelShape CREEPER_EAST_SHAPE = VEBoxUtil.rotate90(CREEPER_NORTH_SHAPE);
+    protected static final VoxelShape NORTH_SHAPE = VoxelShapes.or(NORTH_FEET, NORTH_EYES, NORTH_MOUTH, NORTH_BODY);
+    protected static final VoxelShape SOUTH_SHAPE = VEBoxUtil.rotate180(NORTH_SHAPE);
+    protected static final VoxelShape WEST_SHAPE = VEBoxUtil.rotate270(NORTH_SHAPE);
+    protected static final VoxelShape EAST_SHAPE = VEBoxUtil.rotate90(NORTH_SHAPE);
 
     public VECreeperPlushBlock(Properties properties)
     {
@@ -46,6 +43,6 @@ public class VECreeperPlushBlock extends VEPlushBlock
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context)
     {
-        return defineShapes(state, CREEPER_NORTH_SHAPE, CREEPER_SOUTH_SHAPE, CREEPER_WEST_SHAPE, CREEPER_WEST_SHAPE);
+        return defineShapes(state, NORTH_SHAPE, SOUTH_SHAPE, WEST_SHAPE, EAST_SHAPE);
     }
 }
